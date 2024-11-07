ff8a6838ef79b59dbacafbbc8d7fb4ac8307b6ea
Fernando Batista <fernandohsbatista@gmail.com>
2024-11-07 12:04:15 -0300

    test(messages-queue):(ensure write writes many messages to correct file)

--- transactions/src/messages_queue/test_core.clj
+++ transactions/src/messages_queue/test_core.clj
@@ -22,5 +22,23 @@
       (is (= "Test message" (first content))))
     (delete-file test-file)))
 
+(deftest test-write-many-messages
+  (let [test-file "test-messages.txt"]
+    (with-open [writer (io/writer test-file :append true)]
+      (.write writer ""))
+    (write "Test message 1" test-file)
+    (write "Test message 2" test-file)
+    (write "Test message 3" test-file)
+    (write "Test message 4" test-file)
+    (let [content (read-file test-file)]
+      (is (= 4 (count content)))
+      (is (= "Test message 1" (first content)))
+      (is (= "Test message 2" (second content)))
+      (is (= "Test message 3" (nth content 2)))
+      (is (= "Test message 4" (nth content 3)))
+      (delete-file test-file))))
+
+
+
 
 
